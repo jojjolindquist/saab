8ad3a80b455fb9cef3af891dca2f32ccc84e0ff4
Stina Åström <stias606@student.liu.se>
2023-09-21 13:30:08 +0000

    Update Tour.cpp

--- lab3/tsp/src/Tour.cpp
+++ lab3/tsp/src/Tour.cpp
@@ -12,7 +12,7 @@
 
 Tour::Tour()
 {
-   //initierar vår första pekare till null
+   firstNode = nullptr;
 }
 
 Tour::Tour(Point a, Point b, Point c, Point d){
@@ -27,12 +27,15 @@ Tour::Tour(Point a, Point b, Point c, Point d){
 
 Tour::~Tour()
 {
-    // TODO: write this member
+    while(firstNode != nullptr){
+        Node* temp = firstNode;
+        firstNode = firstNode->next;
+        delete temp;
+    }
 }
 
 void Tour::show()
 {
-    cout <<  firstNode->toString() << endl;
     Node* currentNode = this->firstNode;
     while (currentNode != nullptr) {
         string pointOutput = currentNode->point.toString();
@@ -46,7 +49,15 @@ void Tour::show()
 
 void Tour::draw(QGraphicsScene *scene)
 {
-    // TODO: write this member
+    Node* currentNode = firstNode;
+    while (currentNode != nullptr) {
+        Point pointToDraw = currentNode->point;
+        pointToDraw.drawTo(currentNode->next->point, scene);
+        currentNode = currentNode->next; // hoppar till nästa nod
+        if (currentNode == firstNode){
+            currentNode = nullptr; //sätter till brytvillkoret
+        }
+    }
 }
 
 int Tour::size()
@@ -83,10 +94,56 @@ double Tour::distance()
 
 void Tour::insertNearest(Point p)
 {
-    // TODO: write this member
+    double smallestDistance = 1.7976931348623157E+308;
+    Node* nearestNode = nullptr;
+    Node* currentNode = firstNode;
+    if (currentNode == nullptr){
+        firstNode = new Node(p,  nullptr);
+        firstNode->next = firstNode;
+    }else{
+        while (currentNode != nullptr) {
+            double distance = p.distanceTo(currentNode->point);
+            if (distance < smallestDistance){
+                smallestDistance = distance;
+                nearestNode = currentNode;
+            }
+            currentNode = currentNode->next; // hoppar till nästa nod
+            if (currentNode == firstNode){
+                currentNode = nullptr; //sätter till brytvillkoret
+            }
+        }
+        Node* nextNode = nearestNode->next;
+        nearestNode->next = new Node(p, nextNode);
+    }
 }
 
 void Tour::insertSmallest(Point p)
 {
-    // TODO: write this member
+    double smallestDistance = 1.7976931348623157E+308;
+    Node* nearestNode = nullptr; // den nod som gav kortast sträcka
+    Node* currentNode = firstNode; // håller koll på nuvarande noden när vi itererar
+    Node* pNode = new Node(p, nullptr); // skapar nod för punkten p
+    if (currentNode == nullptr){ // det är en tom tour
+        firstNode = new Node(p,  nullptr); //första noden är punkten som ska sättas in
+        firstNode->next = firstNode; // den pekar på sig själv
+    }else{
+        while (currentNode != nullptr) {
+            Node* nextNode = currentNode->next; // pekar på noden som kommer efter currentnode
+            pNode->next = nextNode; // sätter att P pekar på det som currentnode pekade på
+            currentNode->next = pNode; // currentnode pekar på p
+            double currentDistance = this->distance(); // räknar ut sträckan på tour
+            if (smallestDistance > currentDistance){ // om vi hittat ny kortast sträcka
+               smallestDistance = currentDistance; // ny kortast sträcka
+               nearestNode = currentNode; // sparar var insättning av noden gav kortast sträcka
+            }
+            currentNode->next = nextNode; // ändrar tillbaka som det var
+            currentNode = currentNode->next; // hoppar till nästa nod
+            if (currentNode == firstNode){
+                currentNode = nullptr; //sätter till brytvillkoret
+            }
+        }
+        Node* temp = nearestNode->next; // sätter in punkten p där den gav kortast sträcka
+        pNode->next = temp;
+        nearestNode->next = pNode;
+    }
 }
